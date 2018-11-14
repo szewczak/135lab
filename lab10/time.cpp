a05f7852a0d5dfa910f9d510b0fab564fecee8e9
NSz <nick.szewczak@gmail.com>
2018-11-14 12:06:59 -0500

    success E

--- lab10/time.cpp
+++ lab10/time.cpp
@@ -2,18 +2,28 @@
 Author: Nick Szewczak
 Course: CSCI-136
 Instructor: Subhadarshi Panda
-Assignment: LAB 10 Task D. Scheduling X after Y?
+Assignment: LAB 10 Task E. Overlapping time slots?
 Add a new function
 
-TimeSlot scheduleAfter(TimeSlot ts, Movie nextMovie);
-The function should produce and return a new TimeSlot for the
-movie nextMovie, scheduled immediately after the time slot ts.
-
-For example, if the movie scheduled in ts starts at 14:10 and
-lasts 120 minutes, then the time slot for the next movie
-should start at exactly 16:10.
+bool timeOverlap(TimeSlot ts1, TimeSlot ts2); 
+The function should return true if the two time slots overlap,
+otherwise return false. (Take into account the starting times
+of the time slots and the duration of the scheduled movies.)
 
 Modify main function to test your code.
+
+Hint: You may use minutesUntil to check which time slot is
+earlier, then find the how long is the interval between their
+starting times. They overlap if the movie duration is greater
+than the interval between the time slots’ starting times.
+Alternatively, converting times into minutes since midnight
+can be a good idea as well.
+
+(By the way, if you want to be accurate, if one movie starts
+at 10:00 and lasts 90 minutes until 11:30, then it does not
+overlap with a movie that starts exactly at 11:30. However,
+they would overlap if the latter movie started one minute
+earlier, at 11:29.)
 */
 
 #include <iostream>
@@ -87,6 +97,35 @@ void printTimeSlot(TimeSlot ts){
 TimeSlot scheduleAfter(TimeSlot ts, Movie nextMovie){
     return TimeSlot {nextMovie, addMinutes(ts.startTime, ts.movie.duration)};
 }
+
+bool AbeforeB (Time a, Time b){
+    if(a.h == b.h){
+        if(a.m < b.m){
+            return true;
+        }
+    }
+    if(a.h < b.h){
+        return true;
+    }
+    else return false;
+}
+
+bool timeOverlap(TimeSlot ts1, TimeSlot ts2){
+    if(AbeforeB(ts1.startTime,ts2.startTime)){
+        Time alpha = addMinutes(ts1.startTime, ts1.movie.duration);
+        if(AbeforeB(ts2.startTime, alpha)){
+            return true;
+        }
+    }
+    else{
+        Time alpha = addMinutes(ts2.startTime, ts2.movie.duration);
+        if(AbeforeB(ts1.startTime, alpha)){
+            return true;
+        }
+    }
+    return false;
+}
+
 int main(){
     return 0;
 }
