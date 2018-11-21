7cffd0e8ed90be120dd15c5aa6a69f40c9a61654
NSz <nick.szewczak@gmail.com>
2018-11-21 11:14:51 -0500

    Task B complete

--- Lab11/recursion.cpp
+++ Lab11/recursion.cpp
@@ -2,29 +2,40 @@
     Author: Nick Szewczak
     Course: CSCI-136
     Instructor: Subhadarshi Panda
-    Assignment: Lab 11 Task A Print all numbers in range
-    Write a program recursion.cpp, defining a function
+    Assignment: Lab 11 Task B. Sum of numbers in range
+    In the same program recursion.cpp, add a function
 
-    void printRange(int left, int right);
-    that prints all numbers in range left ≤   x   ≤ right, 
-    separated by spaces. (Don’t use loops, global or static
-    variables.)
+    int sumRange(int left, int right);
+    that computes the sum of all numbers in range left ≤   x   ≤ right. (Don’t use loops, global or static variables.)
 
     A usage example:
 
-        int main() {
-            printRange(-2, 10);
-        }
-        Will print:
+    int main() {
+        int x = sumRange(1, 3);
+        cout << This is << x << endl;   // 6
+        
+        int y = sumRange(-2, 10);
+        cout << That is << y << endl;   // 52
+    }
+    What makes it different from the previous example, this function has to return the answer:
 
-        -2 -1 0 1 2 3 4 5 6 7 8 9 10
-    When left > right, the range is empty and the program should 
-    not print any numbers.
+    if the range is empty, the sum is zero.
+    Otherwise sum(left, right) = left + sum(left + 1, right).
 */
 
 #include <iostream>
 
 using namespace std;
+
+int sumRange(int left, int right){
+    if(left > right){
+        return 0;
+    }
+    if(left < right){
+        return left + sumRange(left+1, right);
+    }
+    else return left;
+}
 
 void printRange(int left, int right){
     if(left <= right){
@@ -34,6 +45,6 @@ void printRange(int left, int right){
 }
 
 int main(){
-    printRange(-2,10);
+    cout << sumRange(-2,10) << endl;
     return 0;
 }
