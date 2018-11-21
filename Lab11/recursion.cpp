ec3719def1e2044040222df9bbba3c9a81a256c7
NSz <nick.szewczak@gmail.com>
2018-11-21 11:25:21 -0500

    Task C Completed

--- Lab11/recursion.cpp
+++ Lab11/recursion.cpp
@@ -2,30 +2,62 @@
     Author: Nick Szewczak
     Course: CSCI-136
     Instructor: Subhadarshi Panda
-    Assignment: Lab 11 Task B. Sum of numbers in range
-    In the same program recursion.cpp, add a function
+    Assignment: Lab 11 Task C. Sum of elements in array
+        In the same program, add a new function
 
-    int sumRange(int left, int right);
-    that computes the sum of all numbers in range left ≤   x   ≤ right. (Don’t use loops, global or static variables.)
+        int sumArray(int *arr, int size);
+        which receives an array (as a pointer to its first element) and the size of the array, and should return the sum of its elements. The function itself should not do any new dynamic memory allocations.
 
-    A usage example:
+        There are several approaches to this task:
 
-    int main() {
-        int x = sumRange(1, 3);
-        cout << This is << x << endl;   // 6
-        
-        int y = sumRange(-2, 10);
-        cout << That is << y << endl;   // 52
-    }
-    What makes it different from the previous example, this function has to return the answer:
+        One possible strategy is to define a helper function
+        sumArrayInRange(int *arr, int left, int right);
+        which adds up all elements of the passed array, but only for indexes in the interval left ≤ i ≤ right. It can be implemented very similarly to the function sumRange, but it should be adding the elements of the array instead of range indices. 
+        Then sumArray(arr, size) can be defined as 
+        sumArrayInRange(arr, 0, size-1).
 
-    if the range is empty, the sum is zero.
-    Otherwise sum(left, right) = left + sum(left + 1, right).
+        Alternatively, can you maybe get away with just using the original function?
+        A usage example:
+
+        int main() {
+
+            int size = 10;
+            int *arr = new int[size]; // allocate array dynamically
+            arr[0] = 12;
+            arr[1] = 17;
+            arr[2] = -5;
+            arr[3] = 3;
+            arr[4] = 7;
+            arr[5] = -15;
+            arr[6] = 27;
+            arr[7] = 5;
+            arr[8] = 13;
+            arr[9] = -21;
+
+            int sum1 = sumArray(arr, size); // Add all elements
+            cout << "Sum is " << sum1 << endl;  // Sum is 43
+            
+            int sum2 = sumArray(arr, 5); // Add up first five elements
+            cout << "Sum is " << sum2 << endl;  // Sum is 34
+
+            delete[] arr;         // deallocate it
+        }
 */
 
 #include <iostream>
 
 using namespace std;
+
+int sumArrayWIndex(int *arr, int i, int size){
+    if(i < size){
+        return arr[i] + sumArrayWIndex(arr, i+1, size);
+    }
+    else return 0;
+}
+
+int sumArray(int *arr, int size){
+    return sumArrayWIndex(arr, 0, size);
+}
 
 int sumRange(int left, int right){
     if(left > right){
